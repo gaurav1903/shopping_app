d2e04130e517498306d47398417cf36d891766d4
gaurav arora <gaurav1903arora@gmail.com>
2021-02-14 01:45:10 +0530

    auto logout

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import '../models/exception.dart';
+import 'dart:async';
 
 class Auth with ChangeNotifier {
   String _token;
   DateTime _expirydate;
   String _userid;
+  Timer _authtimer;
   Future<void> signup(String email, String password) async {
     const url =
         "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDa585-C2kbUd9nC1RDUg297Ys9nus9UO4";
@@ -26,16 +28,30 @@ class Auth with ChangeNotifier {
       _userid = responsedata['localId'];
       _expirydate = DateTime.now()
           .add(Duration(seconds: int.parse(responsedata['expiresIn'])));
+      autologout();
       notifyListeners();
     } catch (e) {
       throw e;
     }
   }
 
+  void autologout() {
+    if (_authtimer != null) {
+      _authtimer.cancel();
+      _authtimer = null;
+    }
+    final secs = _expirydate.difference(DateTime.now()).inSeconds;
+    _authtimer = Timer(Duration(seconds: secs), logout);
+  }
+
   void logout() {
     _token = null;
     _userid = null;
     _expirydate = null;
+    if (_authtimer != null) {
+      _authtimer.cancel();
+      _authtimer = null;
+    }
     notifyListeners();
   }
 
@@ -59,6 +75,7 @@ class Auth with ChangeNotifier {
       _expirydate = DateTime.now()
           .add(Duration(seconds: int.parse(responsedata['expiresIn'])));
       print("token $_token");
+      autologout();
       notifyListeners();
     } catch (e) {
       throw e;
