dfd2e4734d176c76e9e804f70d45fa3048977951
gaurav arora <gaurav1903arora@gmail.com>
2021-02-15 20:35:06 +0530

    okk

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -19,7 +19,7 @@ class ProductItem extends StatelessWidget {
                 .pushNamed('/productdetail', arguments: product.id);
           },
           child: FadeInImage(
-            placeholder: AssetImage('assets/images/placeholder.png'),
+            placeholder: AssetImage('assets/images/placeholder.jpg'),
             image: NetworkImage(
               product.imageurl,
             ),
