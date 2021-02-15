b031def95404422dfdcd8f14625c2e488c4fbdb4
gaurav arora <gaurav1903arora@gmail.com>
2021-02-15 23:11:07 +0530

    scroll animation

--- lib/screens/product_detail.dart
+++ lib/screens/product_detail.dart
@@ -9,41 +9,51 @@ class ProductDetail extends StatelessWidget {
     final selectedproduct =
         Provider.of<ProductData>(context, listen: false).findbyid(id);
     return Scaffold(
-      appBar: AppBar(
-        title: Text(selectedproduct.title),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-                width: double.infinity,
-                child: Hero(
-                  tag: selectedproduct.id,
-                  child: Image.network(
-                    selectedproduct.imageurl,
-                    fit: BoxFit.cover,
-                  ),
-                )),
-            SizedBox(height: 10),
-            Text(
-              '${selectedproduct.price}',
-              style: TextStyle(color: Colors.grey, fontSize: 20),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              width: double.infinity,
-              padding: EdgeInsets.symmetric(horizontal: 10),
-              child: Text(
-                selectedproduct.description,
-                textAlign: TextAlign.center,
-                softWrap: true,
+      // appBar: AppBar(
+      //   title: Text(selectedproduct.title),
+      // ),
+      body: CustomScrollView(slivers: [
+        SliverAppBar(
+          expandedHeight: 300,
+          pinned: true,
+          flexibleSpace: FlexibleSpaceBar(
+            title: Text(selectedproduct.title),
+            background: Hero(
+              tag: selectedproduct.id,
+              child: Image.network(
+                selectedproduct.imageurl,
+                fit: BoxFit.contain,
               ),
-            )
-          ],
+            ),
+          ),
         ),
-      ),
+        SliverList(
+            delegate: SliverChildListDelegate([
+          SizedBox(height: 10),
+          Text(
+            '${selectedproduct.price}',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 20,
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+            width: double.infinity,
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: Text(
+              selectedproduct.description,
+              style: TextStyle(fontSize: 20),
+              textAlign: TextAlign.center,
+              softWrap: true,
+            ),
+          ),
+          SizedBox(height: 600),
+        ]))
+      ]),
     );
   }
 }
